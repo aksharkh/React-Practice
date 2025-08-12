6ee1229d1ca7213403211db33c34df9cdbb8140e
aksharkh <aksharkh04@gmail.com>
2025-08-12 17:35:23 +0530

    added vite file

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+
+      <p className="text-xl bg-black text-white p-4 rounded-lg">
+        Click on the Vite and React logos to learn more
+      </p>
+
+  )
+}
+
+export default App
